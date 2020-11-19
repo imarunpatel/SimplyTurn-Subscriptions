a96e2ca977e5dc18d72e5f5aad13ab2d2ff8e7ef
Arun Patel <arunpatel209@gmail.com>
2020-11-19 13:50:36 +0530

    Added RouterLink in footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,56 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   windowScrolled: boolean;
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-  @HostListener("window:scroll", [])
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {}
+  @HostListener('window:scroll', [])
   onWindowScroll() {
-      if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
-          this.windowScrolled = true;
-      } 
-     else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
-          this.windowScrolled = false;
-      }
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
   }
   scrollToTop() {
-      (function smoothscroll() {
-          var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-          if (currentScroll > 0) {
-              window.requestAnimationFrame(smoothscroll);
-              window.scrollTo(0, currentScroll - (currentScroll / 8));
-          }
-      })();
+    (function smoothscroll() {
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
   }
-
 
   ngOnInit(): void {
     window.onscroll = function () {
       if (pageYOffset >= 1000) {
-          document.getElementById('back-to-top').style.visibility = "visible";
+        document.getElementById('back-to-top').style.visibility = 'visible';
       } else {
-   document.getElementById('back-to-top').style.visibility = "hidden";
+        document.getElementById('back-to-top').style.visibility = 'hidden';
       }
-  };
-  
+    };
+
+    this.router.events.subscribe((evt) => {
+        if (!(evt instanceof NavigationEnd)) {
+            return;
+        }
+        window.scrollTo(0, 0)
+    });
   }
 }
